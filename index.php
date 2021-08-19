7c3638e3fd3cb49459d92fd6fda668abe480f4e5
maite <anayamaiteabril@gmail.com>
2021-08-18 21:15:27 -0300

    git pull

---
+++ index.php
@@ -0,0 +1,194 @@
+<?php
+   
+?>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>FULLFENICIA &mdash; </title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+   <meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+   <meta name="author" content="FreeHTML5.co" />
+
+   <!-- Facebook and Twitter integration -->
+   <meta property="og:title" content=""/>
+   <meta property="og:image" content=""/>
+   <meta property="og:url" content=""/>
+   <meta property="og:site_name" content=""/>
+   <meta property="og:description" content=""/>
+   <meta name="twitter:title" content="" />
+   <meta name="twitter:image" content="" />
+   <meta name="twitter:url" content="" />
+   <meta name="twitter:card" content="" />
+
+   <!-- Bootstrap  -->
+   <link rel="stylesheet" href="css/bootstrap.css">
+   <!-- Owl Carousel  -->
+   <link rel="stylesheet" href="css/owl.carousel.css">
+   <link rel="stylesheet" href="css/owl.theme.default.min.css">
+   <!-- Animate.css -->
+   <link rel="stylesheet" href="css/animate.css">
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+   <!-- Theme style  -->
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+<div id="page-wrap">
+
+
+   <!-- ==========================================================================================================
+                                          HERO
+       ========================================================================================================== -->
+
+   <div id="fh5co-hero-wrapper">
+      <nav class="container navbar navbar-expand-lg main-navbar-nav navbar-light">
+         <a class="navbar-brand" href="#"><img class="google-play-btn" src="img/logoa.png" ></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+
+         
+      </nav>
+
+      <div class="container fh5co-hero-inner">
+         <h1 class="animated fadeIn wow" data-wow-delay="0.4s">DONDE TODAS LAS OPERACIONES SON POSIBLES</h1>
+         
+         <button class="btn btn-md download-btn-first wow fadeInLeft animated" data-wow-delay="0.85s" onclick="$('#fh5co-download').goTo();return false;">Contacto</button> 
+         <img class="fh5co-hero-smartphone animated fadeInRight wow" data-wow-delay="1s" src="img/celu.svg" alt="Smartphone">
+      </div>
+
+
+   </div> <!-- first section wrapper -->
+
+
+   
+
+   <!-- ==========================================================================================================
+                                         SLIDER
+       ========================================================================================================== -->
+
+   <div class="fh5co-slider-outer wow fadeIn" data-wow-delay="0.36s">
+      <h1>Publicar, comprar, intercambiar productos o servicios <br>en un solo lugar </h1>
+      
+      <div class="container fh5co-slider-inner">
+
+         <div class="owl-carousel owl-theme">
+
+            <div class="item"><img src="img/smartphone-3.png" alt=""></div>
+            <div class="item"><img src="img/smartphone-2.png" alt=""></div>
+            <div class="item"><img src="img/iphone.png" alt=""></div>
+            <div class="item"><img src="img/smartphone-1.png" alt=""></div>
+
+
+         </div>
+
+      </div>
+   </div>
+
+
+   <!-- ==========================================================================================================
+                                         FEATURES
+       ========================================================================================================== -->
+
+   <div class="curved-bg-div wow animated fadeIn" data-wow-delay="0.15s"></div>
+   <div id="fh5co-features" class="fh5co-features-outer">
+      <div class="container">
+
+         <div class="row fh5co-features-grid-columns">
+
+            <div class="col-sm-6 in-order-1 wow animated fadeInLeft" data-wow-delay="0.22s">
+               <div class="col-sm-image-container">
+                  <img class="img-float-left" src="img/smartphone-1.png" alt="smartphone-1">
+                  
+               </div>
+            </div>
+
+            <div class="col-sm-6 in-order-2 sm-6-content wow animated fadeInRight" data-wow-delay="0.22s">
+               <h1>Ofrece tus productos o servicios</h1>
+               
+            </div>
+
+            <div class="col-sm-6 in-order-3 sm-6-content wow animated fadeInLeft" data-wow-delay="0.22s">
+               <h1>Intercambia con otros usuarios</h1>
+               
+            </div>
+
+            <div class="col-sm-6 in-order-4 wow animated fadeInRight" data-wow-delay="0.22s">
+               <img class="img-float-right" src="img/smartphone-2.png" alt="smartphone-2">
+            </div>
+
+            <div class="col-sm-6 in-order-5 wow animated fadeInLeft" data-wow-delay="0.22s">
+               <div class="col-sm-image-container">
+                  <img class="img-float-left" src="img/smartphone-3.png" alt="smartphone-3">
+                  
+               </div>
+            </div>
+            <div class="col-sm-6 in-order-6 sm-6-content wow animated fadeInRight" data-wow-delay="0.22s">
+               <h1>ENCONTRA LO QUE ESTAS BUSCANDO</h1>
+               
+            </div>
+
+
+
+
+         </div> <!-- row -->
+
+
+      </div>
+   </div>
+
+
+   <!-- ==========================================================================================================
+                                                 BOTTOM
+    ========================================================================================================== -->
+
+   <div id="fh5co-download" class="fh5co-bottom-outer">
+      <div class="overlay">
+         <div class="container fh5co-bottom-inner">
+            <div class="row">
+               <div class="col-sm-6">
+                  <h1>Proximamente en stores</h1>
+                  
+                  <a class="wow fadeIn animated" data-wow-delay="0.25s" href="#"><img class="app-store-btn" src="img/app-store-icon.png" alt="App Store Icon"></a>
+                  <a class="wow fadeIn animated" data-wow-delay="0.67s" href="#"><img class="google-play-btn" src="img/google-play-icon.png" alt="Google Play Icon"></a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <!-- ==========================================================================================================
+                                               SECTION 7 - SUB FOOTER
+    ========================================================================================================== -->
+
+   <footer class="footer-outer">
+      <div class="container footer-inner">
+
+         <div class="footer-three-grid wow fadeIn animated" data-wow-delay="0.66s">
+            <div class="column-1-3">
+               <a href="#"><img class="google-play-btn" src="img/logo.png" ></a>
+               
+            </div>
+            <p class="copyright">&copy; 2021 FULLFENICIA. All rights reserved. Design by <a href="https://collectmkt.com/" target="_blank">Collectmkt</a>.</p>
+         </div>
+
+      </div>
+   </footer>
+
+
+
+
+</div> <!-- main page wrapper -->
+   
+   <script src="js/jquery.min.js"></script>
+   <script src="js/bootstrap.js"></script>
+   <script src="js/owl.carousel.js"></script>
+   <script src="js/wow.min.js"></script>
+   <script src="js/main.js"></script>
+</body>
+</html>
